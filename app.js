d85359bd454dc510d4cab9e032f90a7b3d988c0e
Kubilay Türe <kubilayture@gmail.com>
2024-10-04 23:37:03 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import postRoute from './routes/post.route.js';
+import authRoute from './routes/auth.route.js';
+
+const app = express();
+
+app.use("api/posts", postRoute);
+app.use("api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log('server is running');
+});
