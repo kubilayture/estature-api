d222b65c6228614f33aaa3ed16406b629aba3292
Kubilay Türe <kubilayture@gmail.com>
2024-10-10 00:18:32 +0300

    prisma and mongodb setup, register and login

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("api/posts", postRoute);
-app.use("api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log('server is running');
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,69 @@
-export const register = (req, res) => {
-  // db operations
-  console.log('register endpoint');
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
+import jwt from 'jsonwebtoken';
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Required fields is missing' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword
+      }
+    });
+
+    if (newUser) {
+      res.status(201).json({ message: 'User created successfully' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Failed to create user' });
+  }
 };
-export const login = (req, res) => {
-  // db operations
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username
+      }
+    });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid Credentials' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid Credentials' });
+    }
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign({
+      id: user.id
+    }, process.env.JWT_SECRET_KEY,
+      { expiresIn: age });
+
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true
+      maxAge: age
+    }).status(200).json({ message: 'Logged in successfully' });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to login' });
+  }
 };
 export const logout = (req, res) => {
-  // db operations
+  res.clearCookie("token").status(200).json({ message: "Logged out successfully" });
 };
