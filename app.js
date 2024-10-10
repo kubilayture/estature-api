444993ebccb87f6d37697706d0df6af6519979aa
Kubilay Türe <kubilayture@gmail.com>
2024-10-10 22:47:02 +0300

    user controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log('server is running');
